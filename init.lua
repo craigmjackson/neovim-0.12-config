22370ee075a3deae29f2eade44ce38591dae820f
Craig Jackson <craigmjackson@email.com>
2026-05-16 22:23:14 -0700

    Added indentation settings

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
 -- Set leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 -- Leader key workaround
-vim.keymap.set('n', '<leader>s', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", "<Nop>", { noremap = true, silent = true })
 -- Press Esc to cancel search highlight
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { noremap=true })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { noremap = true })
 -- Enable line numbers
 vim.opt.number = true
 -- Enable relative line numbers
 vim.opt.relativenumber = true
 -- Enable mouse
-vim.opt.mouse = 'nvi'
+vim.opt.mouse = "nvi"
 -- Sync with OS clipboard
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = "unnamedplus"
 end)
 -- Make undo files
 vim.opt.undofile = true
@@ -22,7 +22,7 @@ vim.opt.ignorecase = true
 -- If there is a mixed case in your search, make it case-sensitive
 vim.opt.smartcase = true
 -- Only show sign column if needed
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 -- Write swapfile to disk if no activity for 250ms
 vim.opt.updatetime = 250
 -- Timeout for other sequences
@@ -43,6 +43,13 @@ vim.opt.winblend = 30
 vim.g.nerd_font = true
 -- Manage NPM packages
 vim.g.manage_npm = true
+-- General indentation settings
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = -1
+vim.o.smarttab = true
+vim.o.smartindent = true
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -64,87 +71,92 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 vim.api.nvim_exec_autocmds("BufEnter", { buffer = 0 })
 
 -- Install plugins if we can connect to github
-require('plugins')
+require("plugins")
 
 -- neovim lsp integration
-local lazydev_ok, lazydev = pcall(require, 'lazydev')
+local lazydev_ok, lazydev = pcall(require, "lazydev")
 if lazydev_ok then
   lazydev.setup()
 end
-vim.keymap.set('n', 'gd', vim.lsp.buf.implementation)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.implementation)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 -- lua_ls
 pcall(function()
-  vim.lsp.enable({ 'lua_ls' })
+  vim.lsp.enable({ "lua_ls" })
 end)
 
 -- rust_analyzer
 pcall(function()
-  vim.lsp.enable('rust_analyzer')
+  vim.lsp.enable("rust_analyzer")
 end)
 
--- pyright
+-- python
 pcall(function()
-  vim.lsp.enable('basedpyright')
+  vim.lsp.enable("basedpyright")
+  vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("PythonIndent", { clear = true }),
+    pattern = { "python" },
+    command = "setlocal shiftwidth=4 tabstop=4",
+  })
 end)
 
 -- bash
 pcall(function()
-  vim.lsp.enable('bashls')
+  vim.lsp.enable("bashls")
 end)
 
 -- javascript
 pcall(function()
-local global_node_modules = vim.fn.trim(vim.fn.system('npm root -g'))
-local vue_plugin_path = global_node_modules .. '/@vue/language-server'
-vim.lsp.config('vtsls', {
-  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue' },
-  settings = {
-    javascript = {
-      inlayHints = {
-        parameterNames = { enabled = "all" }, -- Options: "none", "literals", "all"
-        parameterTypes = { enabled = true },
-        variableTypes = { enabled = true },
-        propertyDeclarationTypes = { enabled = true },
-        functionLikeReturnTypes = { enabled = true },
-        enumMemberValues = { enabled = true },
+  local global_node_modules = vim.fn.trim(vim.fn.system("npm root -g"))
+  local vue_plugin_path = global_node_modules .. "/@vue/language-server"
+  vim.lsp.config("vtsls", {
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+    settings = {
+      javascript = {
+        inlayHints = {
+          parameterNames = { enabled = "all" }, -- Options: "none", "literals", "all"
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+      typescript = {
+        inlayHints = {
+          parameterNames = { enabled = "all" },
+          parameterTypes = { enabled = true },
+          variableTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          functionLikeReturnTypes = { enabled = true },
+          enumMemberValues = { enabled = true },
+        },
+      },
+      vtsls = {
+        tsserver = {
+          globalPlugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_plugin_path,
+              languages = { "vue" },
+              configNamespace = "typescript",
+              enableForWorkspaceTypescriptVersions = true,
+            },
+          },
+        },
       },
     },
-    typescript = {
-      inlayHints = {
-        parameterNames = { enabled = "all" },
-        parameterTypes = { enabled = true },
-        variableTypes = { enabled = true },
-        propertyDeclarationTypes = { enabled = true },
-        functionLikeReturnTypes = { enabled = true },
-        enumMemberValues = { enabled = true },
-      },
-    },
-    vtsls = {
-      tsserver = {
-        globalPlugins = {
-          {
-            name = '@vue/typescript-plugin',
-	    location = vue_plugin_path,
-	    languages = { 'vue' },
-	    configNamespace = 'typescript',
-	    enableForWorkspaceTypescriptVersions = true
-	  }
-	}
-	     }
-	   }
-  },
-})
-  vim.lsp.enable('vtsls')
+  })
+  vim.lsp.enable("vtsls")
 end)
 
 -- vue
 pcall(function()
-  vim.lsp.config('vue_ls', {
-    filetypes = { 'vue' }
+  vim.lsp.config("vue_ls", {
+    filetypes = { "vue" },
   })
-  vim.lsp.enable('vue_ls')
+  vim.lsp.enable("vue_ls")
 end)
 
 -- markdown
@@ -152,7 +164,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*.md",
   callback = function(opts)
     pcall(function()
-      local render_markdown = require('render-markdown')
+      local render_markdown = require("render-markdown")
       render_markdown.setup({
         completions = {
           lsp = {
@@ -169,45 +181,46 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 -- json
 pcall(function()
-  vim.lsp.enable('jsonls')
+  vim.lsp.enable("jsonls")
 end)
 
 -- html
 pcall(function()
-  vim.lsp.enable('html')
+  vim.lsp.enable("html")
 end)
 
 -- css
 pcall(function()
-  vim.lsp.enable('cssls')
+  vim.lsp.enable("cssls")
 end)
 
 -- sql
 pcall(function()
-  vim.lsp.enable('sqlls')
+  vim.lsp.enable("sqlls")
 end)
 
 -- yaml
 pcall(function()
-  vim.lsp.enable('yamlls')
+  vim.lsp.enable("yamlls")
 end)
 
 -- Autocomplete
 vim.o.autocomplete = true
-vim.opt.complete:append('o')
-vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.complete:append("o")
+vim.opt.completeopt = { "menuone", "noselect" }
 vim.o.pumheight = 10
-vim.o.pumborder = 'rounded'vim.api.nvim_create_autocmd('LspAttach', {
+vim.o.pumborder = "rounded"
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, event.buf, {autotrigger = true})
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
     end
-  end
+  end,
 })
 
 -- Inlay hints
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     -- Check the native capabilities table directly instead of calling a method
@@ -220,32 +233,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 -- Diagnostics
 vim.o.updatetime = 300
-vim.api.nvim_create_autocmd('CursorHold', {
+vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
     local opts = {
       focusable = false,
-      close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
-      border = 'rounded',
-      source = 'always',
-      prefix = '',
-      scope = 'cursor'
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = "",
+      scope = "cursor",
     }
     vim.diagnostic.open_float(nil, opts)
-  end
+  end,
 })
 
 -- Theme
 pcall(function()
-  local catppuccin = require('catppuccin')
+  local catppuccin = require("catppuccin")
   if catppuccin then
     catppuccin.setup()
-    vim.cmd('colorscheme catppuccin-macchiato')
+    vim.cmd("colorscheme catppuccin-macchiato")
   end
 end)
 
 -- Icons
 pcall(function()
-  local nvim_web_devicons = require('nvim-web-devicons')
+  local nvim_web_devicons = require("nvim-web-devicons")
   if nvim_web_devicons then
     nvim_web_devicons.setup()
   end
@@ -253,89 +266,265 @@ end)
 
 -- Filemanager
 pcall(function()
-  local nvim_tree = require('nvim-tree')
+  local nvim_tree = require("nvim-tree")
   if nvim_tree then
     nvim_tree.setup({
-    disable_netrw = true,
-    hijack_netrw = true,
-    filters = {
-      git_ignored = false,
-    },
-    actions = {
-      open_file = {
-        quit_on_open = true,
+      disable_netrw = true,
+      hijack_netrw = true,
+      filters = {
+        git_ignored = false,
       },
-    },
-    renderer = {
-      icons = {
-        web_devicons = {
-          file = {
-            enable = vim.g.nerd_font,
-            color = true,
-          },
-          folder = {
-            enable = vim.g.nerd_font,
-            color = true,
-          },
-        },
-        glyphs = vim.g.nerd_font and {} or {
-          default = "",
-          symlink = "",
-          bookmark = "",
-          modified = "m",
-          hidden = "",
-          folder = {
-            arrow_closed = ">",
-            arrow_open = "v",
-            default = "d",
-            open = "d",
-            empty = "d",
-            empty_open = "d",
-            symlink = "ds",
-            symlink_open = "ds",
-          },
-          git = {
-            unstaged = "",
-            staged = "s",
-            unmerged = "",
-            untracked = "",
-            deleted = "d",
-            ignored = "",
-          },
+      actions = {
+        open_file = {
+          quit_on_open = true,
         },
       },
-    },
+      renderer = {
+        icons = {
+          web_devicons = {
+            file = {
+              enable = vim.g.nerd_font,
+              color = true,
+            },
+            folder = {
+              enable = vim.g.nerd_font,
+              color = true,
+            },
+          },
+          glyphs = vim.g.nerd_font and {} or {
+            default = "",
+            symlink = "",
+            bookmark = "",
+            modified = "m",
+            hidden = "",
+            folder = {
+              arrow_closed = ">",
+              arrow_open = "v",
+              default = "d",
+              open = "d",
+              empty = "d",
+              empty_open = "d",
+              symlink = "ds",
+              symlink_open = "ds",
+            },
+            git = {
+              unstaged = "",
+              staged = "s",
+              unmerged = "",
+              untracked = "",
+              deleted = "d",
+              ignored = "",
+            },
+          },
+        },
+      },
     })
   end
-  vim.keymap.set('n', '<c-n>', ':NvimTreeToggle<cr>', { noremap = true })
+  vim.keymap.set("n", "<c-n>", ":NvimTreeToggle<cr>", { noremap = true })
 end)
 
 -- Picker
 pcall(function()
-  local minipick = require('mini.pick')
+  local minipick = require("mini.pick")
   if minipick then
-   	minipick.setup({
-		source = {
-			show = minipick.default_show,
-		},
-		window = {
-			prompt_caret = "|",
-			prompt_prefix = "> ",
-		},
-		show_icons = vim.g.nerd_font,
-	})
-	local pick_neovim_config = function()
-		local picked_filename = minipick.start({
-			source = {
-				items = vim.fn.readdir(vim.fn.stdpath("config")),
-			},
-		})
-		if picked_filename then
-		local filename_with_path = vim.fn.stdpath("config") .. "/" .. picked_filename
-		vim.cmd("edit " .. filename_with_path)
-	end
-	end
-	-- Fuzzy find NeoVim config files
-	vim.keymap.set("n", "<space>sn", pick_neovim_config, { noremap = true }) 
+    minipick.setup({
+      source = {
+        show = minipick.default_show,
+      },
+      window = {
+        prompt_caret = "|",
+        prompt_prefix = "> ",
+      },
+      show_icons = vim.g.nerd_font,
+    })
+    local pick_neovim_config = function()
+      local picked_filename = minipick.start({
+        source = {
+          items = vim.fn.readdir(vim.fn.stdpath("config")),
+        },
+      })
+      if picked_filename then
+        local filename_with_path = vim.fn.stdpath("config") .. "/" .. picked_filename
+        vim.cmd("edit " .. filename_with_path)
+      end
+    end
+    -- Fuzzy find for file in the project
+    vim.keymap.set("n", "<leader>sf", ":Pick files<cr>", { noremap = true })
+    -- Fuzzy find for content in the project
+    vim.keymap.set("n", "<leader>sg", ":Pick grep_live<cr>", { noremap = true })
+    -- Fuzzy find NeoVim config files
+    vim.keymap.set("n", "<leader>sn", pick_neovim_config, { noremap = true })
+  end
+end)
+
+-- Git signs
+pcall(function()
+  local git_signs = require("gitsigns")
+  if git_signs then
+    local opts = vim.g.nerd_font and {}
+      or {
+        signs = {
+          add = { text = "|" },
+          change = { text = "|" },
+          delete = { text = "_" },
+          topdelete = { text = "_" },
+          changedelete = { text = "~" },
+          untracked = { text = " " },
+        },
+        signs_staged = {
+          add = { text = "|" },
+          change = { text = "|" },
+          delete = { text = "_" },
+          topdelete = { text = "_" },
+          changedelete = { text = "~" },
+          untracked = { text = " " },
+        },
+      }
+    git_signs.setup(opts)
+  end
+end)
+
+-- Statusline
+pcall(function()
+  local statusline = require("mini.statusline")
+  if statusline then
+    statusline.setup({
+      use_icons = vim.g.nerd_font,
+    })
+  end
+end)
+
+-- Breadcrumbs
+pcall(function()
+  local dropbar = require("dropbar")
+  if dropbar then
+    dropbar.setup({
+      dropbar.setup({
+        icons = vim.g.nerd_font and {} or {
+          kinds = {
+            symbols = {
+              Array = "(arr)",
+              BlockMappingPair = "(blkmappair)",
+              Boolean = "(bool)",
+              BreakStatement = "(brk)",
+              Call = "(call)",
+              CaseStatement = "(case)",
+              Class = "(cls)",
+              Constant = "(const)",
+              Constructor = "(constr)",
+              ContinueStatment = "(cont)",
+              Copilot = "(copilot)",
+              Declaration = "(decl)",
+              Delete = "(del)",
+              DoStatement = "(do)",
+              Element = "(elem)",
+              Enum = "(enum)",
+              EnumMember = "(emumMem)",
+              Event = "(evt)",
+              Field = "(fld)",
+              File = "(f)",
+              Folder = "(d)",
+              ForStatement = "(for)",
+              Function = "(fn)",
+              GotoStatement = "(goto)",
+              Identifier = "(ident)",
+              IfStatement = "(if)",
+              Interface = "(intf)",
+              Keyword = "(kwd)",
+              List = "(list)",
+              Log = "(log)",
+              Lsp = "(lsp)",
+              Macro = "(mac)",
+              MarkdownH1 = "(h1)",
+              MarkdownH2 = "(h2)",
+              MarkdownH3 = "(h3)",
+              MarkdownH4 = "(h4)",
+              MarkdownH5 = "(h5)",
+              MarkdownH6 = "(h6)",
+              Method = "(mth)",
+              Module = "(mod)",
+              Namespace = "(ns)",
+              Null = "(nul)",
+              Number = "(num)",
+              Object = "(obj)",
+              Operator = "(oper)",
+              Package = "(pkg)",
+              Pair = "(pair)",
+              Property = "(prop)",
+              Reference = "(ref)",
+              Regex = "(regex)",
+              Repeat = "(rep)",
+              Return = "(ret)",
+              Rule = "(rule)",
+              RuleSet = "(ruleset)",
+              Scope = "(scope)",
+              Section = "(sec)",
+              Snippet = "(snip)",
+              Specifier = "(spec)",
+              Statement = "(stmt)",
+              String = "(str)",
+              Struct = "(struct)",
+              SwitchStatement = "(swit)",
+              Table = "(tbl)",
+              Terminal = "(term)",
+              Text = "(txt)",
+              Type = "(type)",
+              TypeParameter = "(typepar)",
+              Unit = "(unit)",
+              Value = "(val)",
+              Variable = "(var)",
+              WhileStatement = "(whl)",
+            },
+          },
+          ui = {
+            bar = {
+              separator = "> ",
+              extends = "...",
+            },
+            menu = {
+              separator = " ",
+              indicator = "> ",
+            },
+          },
+        },
+      }),
+    })
+  end
+end)
+
+-- Tabs
+pcall(function()
+  local bufferline = require("bufferline")
+  if bufferline then
+    bufferline.setup({
+      options = {
+        nubmers = "ordinal",
+        separator_style = "slant",
+        buffer_close_icon = vim.g.nerd_font and "" or "x",
+        close_icon = vim.g.nerd_font and "" or "x",
+        modified_icon = vim.g.nerd_font and "" or "m",
+        left_trunc_marker = vim.g.nerd_font and "" or "/",
+        right_trunc_marker = vim.g.nerd_font and "" or "\\",
+      },
+    })
+  end
+end)
+
+-- Formatting
+pcall(function()
+  local conform = require("conform")
+  if conform then
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black" },
+        javascript = { "prettier" },
+        vue = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 1000,
+        lsp_format = "fallback",
+      },
+    })
   end
 end)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,22 @@
 local function check_url_simple(url)
-  vim.fn.system({ 'curl', '-Isf', url, '-o', '/dev/null' })
+  vim.fn.system({ "curl", "-Isf", url, "-o", "/dev/null" })
   local success = (vim.v.shell_error == 0)
   return success
 end
 
 -- Install plugins if site is reachable
-if check_url_simple('https://github.com/neovim') then
+if check_url_simple("https://github.com/neovim") then
   vim.pack.add({
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/folke/lazydev.nvim' },
-    { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim' },
-    { src = 'https://github.com/catppuccin/nvim' },
-    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-    { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
-    { src = 'https://github.com/nvim-mini/mini.nvim' }
-  }, {confirm = false})
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/folke/lazydev.nvim" },
+    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+    { src = "https://github.com/catppuccin/nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/nvim-tree/nvim-tree.lua" },
+    { src = "https://github.com/nvim-mini/mini.nvim" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = "https://github.com/Bekaboo/dropbar.nvim" },
+    { src = "https://github.com/akinsho/bufferline.nvim" },
+    { src = "https://github.com/stevearc/conform.nvim" },
+  }, { confirm = false })
 end
